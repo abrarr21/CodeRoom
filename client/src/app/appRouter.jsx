18efd0fa460f0a8336abab7a3753bedcd07ecac5
siva <sivavarma1999@gmail.com>
2026-06-30 10:11:10 +0530

    create UI for register page and room code copy page

--- client/src/app/appRouter.jsx
+++ client/src/app/appRouter.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import RegisterPage from "../pages/RegisterPage";
-import RoomCodePage from "../pages/RoomCodePAge";
+import RoomCodePage from "../pages/RoomCodePage";
 import RoomPage from "../pages/RoomPage";
 
 const router = createBrowserRouter([
